2c204cc9cb4079e61ea5aa79d64c2c301a66d460
Thomas <dahlg136@morris.umn.edu>
2021-09-30 11:29:21 -0500

    Start array_merge.c
    Starting the array_merge.c, it doesn't work yet it's just the preliminary design.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -2,5 +2,26 @@
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
   // This is obviously broken. It has the right type, though, eh?
+  int *bigArray;
+
+      int i;
+      int counter;
+
+      for(i = 0; i < num_arrays; i++){
+          counter = counter + sizes[i];
+      }
+
+  bigArray = (int*)malloc(counter, sizeof(int));
+
+  for(int j = 0; j< num_arrays; j++){
+      int size = sizes[j];
+      for(int k = 0; k<size; k++){
+          bigArray[k]= values[j][k];
+      }
+  }
+
+  for(int h = 0; h < counter; h++){
+      int compare = bigArray[h];
+  }
   return sizes;
 }
