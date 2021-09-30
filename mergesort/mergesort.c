9a46a449db79c928a73dac8a911de2b3c3291ac3
Thomas <dahlg136@morris.umn.edu>
2021-09-30 10:39:05 -0500

    Created merge sort function
    This is the first implementation of merge sort, it doesn't pass all the tests yet.

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,7 +1,66 @@
 #include "mergesort.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
 
-void mergesort(int, int*) {
-  // This obviously doesn't actually do any *sorting*, so there's
-  // certainly work still to be done.
-  return;
+
+int needsSorting(int rangeSize) {
+    int result = 0;
+    if(rangeSize>=2){
+        result = 1;
+    }
+    return result;
+  }
+
+void mergeRanges(int* values, int startIndex, int midPoint, int endIndex) {
+    /*
+     * Assume that the two ranges are sorted:
+     *   (forall i | startIndex <= i <= j < midPoint : values[i] <= values[j])
+     *   (forall i | midPoint <= i <= j < endIndex : values[i] <= values[j])
+     * then merge them into a single sorted array, copy that back, and return.
+     */
+    int rangeSize = endIndex - startIndex;
+    int* destination = (int*)calloc(rangeSize, sizeof(char));
+    int firstIndex = startIndex;
+    int secondIndex = midPoint;
+    int copyIndex = 0;
+    while (firstIndex < midPoint && secondIndex < endIndex) {
+      if (values[firstIndex] < values[secondIndex]) {
+        destination[copyIndex] = values[firstIndex];
+        ++firstIndex;
+      } else {
+        destination[copyIndex] = values[secondIndex];
+        ++secondIndex;
+      }
+      ++copyIndex;
+    }
+    while (firstIndex < midPoint) {
+      destination[copyIndex] = values[firstIndex];
+      ++copyIndex;
+      ++firstIndex;
+    }
+    while (secondIndex < endIndex) {
+      destination[copyIndex] = values[secondIndex];
+      ++copyIndex;
+      ++secondIndex;
+    }
+    for (int i = 0; i < rangeSize; ++i) {
+      values[i + startIndex] = destination[i];
+    }
+  }
+
+void mergesortRange(int* values, int startIndex, int endIndex) {
+    int rangeSize = endIndex - startIndex;
+    if (needsSorting(rangeSize) == 1) {
+      int midPoint = (startIndex + endIndex) / 2;
+      mergesortRange(values, startIndex, midPoint);
+      mergesortRange(values, midPoint, endIndex);
+      mergeRanges(values, startIndex, midPoint, endIndex);
+    }
+  }
+
+  void mergesort(int size, int* values) {
+
+    mergesortRange(values, 0, size);
+
 }
