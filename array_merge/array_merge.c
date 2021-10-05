196f41173ff04ed375d77522352312bddff75b7b
Thomas <dahlg136@morris.umn.edu>
2021-10-05 11:28:42 -0500

    Modulate code of array_merge

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -16,6 +16,17 @@ int inArray(int* array, int number, int size){
     return result;
 }
 
+void intoOneArray(int* bigArray, int** values, int num_arrays, int* sizes){
+  int r = 0;
+  for(int j = 0; j< num_arrays; j++){
+      int size = sizes[j];
+      for(int k = 0; k<size; k++){
+          bigArray[r]= values[j][k];
+          r++;
+      }
+  }
+
+}
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
   int* bigArray;
@@ -32,30 +43,24 @@ int* array_merge(int num_arrays, int* sizes, int** values) {
   bigArray = (int*)calloc(counter, sizeof(int));
   littleArray = (int*)calloc(counter, sizeof(int));
 
-  int r = 0;
-  for(int j = 0; j< num_arrays; j++){
-      int size = sizes[j];
-      for(int k = 0; k<size; k++){
-          bigArray[r]= values[j][k];
-          r++;
-      }
-  }
+  intoOneArray(bigArray, values, num_arrays, sizes);
 
   for(int h = 0; h < counter; h++){
       if(inArray(littleArray, bigArray[h], counter2) == 0){
           littleArray[counter2] = bigArray[h];
-
           counter2++;
       }
   }
 
   mergesort(counter2, littleArray);
+
   finalArray = (int*)calloc(counter2+1, sizeof(int));
   finalArray[0] = counter2;
 
   for(int x = 1; x < counter2+1; x++){
       finalArray[x] = littleArray[x-1];
   }
+
   free(bigArray);
   free(littleArray);
 
