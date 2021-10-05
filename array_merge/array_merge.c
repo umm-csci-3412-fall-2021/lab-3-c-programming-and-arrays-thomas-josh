f29aafc350c52032eeee707f5d79d2ea510f7adc
Thomas <dahlg136@morris.umn.edu>
2021-10-05 11:11:24 -0500

    Finish array merge
    Array merge now works and passes the tests on our end

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,27 +1,63 @@
 #include "array_merge.h"
+#include "../mergesort/mergesort.c"
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int inArray(int* array, int number, int size){
+    int result = 0;
+    for(int i = 0; i < size; i++){
+        if(array[i] == number){
+            result = 1;
+            break;
+        }
+    }
+    return result;
+}
+
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
-  int *bigArray;
+  int* bigArray;
+  int* littleArray;
+  int* finalArray;
+  int counter2 = 0;
+  int i;
+  int counter = 0;
 
-      int i;
-      int counter;
+  for(i = 0; i < num_arrays; i++){
+    counter = counter + sizes[i];
+  }
 
-      for(i = 0; i < num_arrays; i++){
-          counter = counter + sizes[i];
-      }
+  bigArray = (int*)calloc(counter, sizeof(int));
+  littleArray = (int*)calloc(counter, sizeof(int));
 
-  bigArray = (int*)malloc(counter, sizeof(int));
-
+  int r = 0;
   for(int j = 0; j< num_arrays; j++){
       int size = sizes[j];
       for(int k = 0; k<size; k++){
-          bigArray[k]= values[j][k];
+          bigArray[r]= values[j][k];
+          r++;
       }
   }
 
   for(int h = 0; h < counter; h++){
-      int compare = bigArray[h];
+      if(inArray(littleArray, bigArray[h], counter2) == 0){
+          littleArray[counter2] = bigArray[h];
+
+          counter2++;
+      }
   }
-  return sizes;
+
+  mergesort(counter2, littleArray);
+  finalArray = (int*)calloc(counter2+1, sizeof(int));
+  finalArray[0] = counter2;
+
+  for(int x = 1; x < counter2+1; x++){
+      finalArray[x] = littleArray[x-1];
+  }
+  free(bigArray);
+  free(littleArray);
+
+  return finalArray;
 }
